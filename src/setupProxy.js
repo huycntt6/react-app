fb4d9864e949855559ff0f466ee973df7bd2271c
Hoang Van Huy <huynoone@gmail.com>
2020-10-12 11:50:47 +0700

    cm

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,6 +1,5 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function(app) {
-    console.log(app);
   app.use(
     '/.netlify',
     createProxyMiddleware({
